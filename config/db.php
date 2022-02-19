9e7008eed6948a0a7305dfd6cf8bb0eabacb0a15
evsvmx <evsv.mx@gmail.com>
2022-02-19 14:10:04 +0300

    add .env

--- config/db.php
+++ config/db.php
@@ -1,10 +1,17 @@
 <?php
 
+use yii\helpers\ArrayHelper;
+
+$host = ArrayHelper::getValue($_ENV, 'DB_HOST', '127.0.0.1');
+$dbname = ArrayHelper::getValue($_ENV, 'DB_NAME', 'yii2basic');
+$username = ArrayHelper::getValue($_ENV, 'DB_USERNAME', 'root');
+$password = ArrayHelper::getValue($_ENV, 'DB_PASSWORD', '');
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=' . $host . ';dbname=' . $password,
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
