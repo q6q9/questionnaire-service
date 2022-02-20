6fa5a570ea99bb2dcc1ace5c8adc48cbab61ccc0
evsvmx <evsv.mx@gmail.com>
2022-02-20 16:29:32 +0300

    return original

--- web/index.php
+++ web/index.php
@@ -1,23 +1,12 @@
 <?php
-
-// comment out the following two lines when deployed to production
-
 require __DIR__ . '/../vendor/autoload.php';
-
-use yii\helpers\ArrayHelper;
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 try {
     $dotenv = new \Symfony\Component\Dotenv\Dotenv();
     $dotenv->usePutenv(true)->bootEnv(__DIR__ . '/../.env');
 } catch (Exception $e) {
 }
-
-if (ArrayHelper::getValue(getenv(), 'YII_ENV', 'production') == 'dev') {
-    defined('YII_DEBUG') or define('YII_DEBUG', true);
-    defined('YII_ENV') or define('YII_ENV', 'dev');
-}
-
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
 
