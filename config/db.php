dedca79621c15c8949e48b398d2117ab0c8a53ce
evsvmx <evsv.mx@gmail.com>
2022-02-20 15:43:24 +0300

    add DB_DRIVER and Procfile

--- config/db.php
+++ config/db.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\ArrayHelper;
 
+$driver = ArrayHelper::getValue($_ENV, 'DB_DRIVER', 'mysql');
 $host = ArrayHelper::getValue($_ENV, 'DB_HOST', '127.0.0.1');
 $dbname = ArrayHelper::getValue($_ENV, 'DB_NAME', 'yii2basic');
 $username = ArrayHelper::getValue($_ENV, 'DB_USERNAME', 'root');
@@ -9,7 +10,7 @@ $password = ArrayHelper::getValue($_ENV, 'DB_PASSWORD', '');
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=' . $host . ';dbname=' . $dbname,
+    'dsn' => $driver . ':host=' . $host . ';dbname=' . $dbname,
     'username' => $username,
     'password' => $password,
     'charset' => 'utf8',
