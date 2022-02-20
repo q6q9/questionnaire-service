f7a7d856e9c570e454ae3d54ed39283ed8ea2ef4
evsvmx <evsv.mx@gmail.com>
2022-02-20 16:25:45 +0300

    env fix

--- web/index.php
+++ web/index.php
@@ -3,7 +3,6 @@
 // comment out the following two lines when deployed to production
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 use yii\helpers\ArrayHelper;
 
@@ -17,6 +16,8 @@ if (ArrayHelper::getValue(getenv(), 'YII_ENV', 'production') == 'dev') {
     defined('YII_DEBUG') or define('YII_DEBUG', true);
     defined('YII_ENV') or define('YII_ENV', 'dev');
 }
+
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
 
