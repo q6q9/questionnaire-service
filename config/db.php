96607a46f60736edff43ab3addc39d3d5dc4b674
evsvmx <evsv.mx@gmail.com>
2022-02-20 16:00:36 +0300

    getenv()

--- config/db.php
+++ config/db.php
@@ -2,11 +2,11 @@
 
 use yii\helpers\ArrayHelper;
 
-$driver = ArrayHelper::getValue($_ENV, 'DB_DRIVER', 'mysql');
-$host = ArrayHelper::getValue($_ENV, 'DB_HOST', '127.0.0.1');
-$dbname = ArrayHelper::getValue($_ENV, 'DB_NAME', 'yii2basic');
-$username = ArrayHelper::getValue($_ENV, 'DB_USERNAME', 'root');
-$password = ArrayHelper::getValue($_ENV, 'DB_PASSWORD', '');
+$driver = ArrayHelper::getValue(getenv(), 'DB_DRIVER', 'mysql');
+$host = ArrayHelper::getValue(getenv(), 'DB_HOST', '127.0.0.1');
+$dbname = ArrayHelper::getValue(getenv(), 'DB_NAME', 'yii2basic');
+$username = ArrayHelper::getValue(getenv(), 'DB_USERNAME', 'root');
+$password = ArrayHelper::getValue(getenv(), 'DB_PASSWORD', '');
 
 return [
     'class' => 'yii\db\Connection',
--- web/index.php
+++ web/index.php
@@ -1,16 +1,16 @@
 <?php
 
 // comment out the following two lines when deployed to production
-use yii\helpers\ArrayHelper;
-
-if (ArrayHelper::getValue($_ENV, 'YII_ENV', 'production') == 'dev') {
-    defined('YII_DEBUG') or define('YII_DEBUG', true);
-    defined('YII_ENV') or define('YII_ENV', 'dev');
-}
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
+use yii\helpers\ArrayHelper;
+
+if (ArrayHelper::getValue(getenv(), 'YII_ENV', 'production') == 'dev') {
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+}
 
 try {
     $dotenv = new \Symfony\Component\Dotenv\Dotenv();
