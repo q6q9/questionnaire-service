e305d738a3334b44e63b1a153eb1fb2f65c80bed
evsvmx <evsv.mx@gmail.com>
2022-02-20 15:53:56 +0300

    add env YII_ENV

--- web/index.php
+++ web/index.php
@@ -1,8 +1,12 @@
 <?php
 
 // comment out the following two lines when deployed to production
-//defined('YII_DEBUG') or define('YII_DEBUG', true);
-//defined('YII_ENV') or define('YII_ENV', 'dev');
+use yii\helpers\ArrayHelper;
+
+if (ArrayHelper::getValue($_ENV, 'YII_ENV', 'production') == 'dev') {
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+}
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
@@ -10,7 +14,7 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 try {
     $dotenv = new \Symfony\Component\Dotenv\Dotenv();
-    $dotenv->usePutenv(true)->bootEnv(__DIR__.'/../.env');;
+    $dotenv->usePutenv(true)->bootEnv(__DIR__ . '/../.env');
 } catch (Exception $e) {
 }
 
